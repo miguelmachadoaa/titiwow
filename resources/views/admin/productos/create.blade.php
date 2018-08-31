8f4ff2c534b3624211ac3f3126687070aa5267e0
miguelmachadoaa <miguelmachadoaa@gmail.com>
2018-08-31 10:45:28 -0400

    ajuste js de add yy edit productos

--- resources/views/admin/productos/create.blade.php
+++ resources/views/admin/productos/create.blade.php
@@ -385,7 +385,7 @@ Nuevo Producto
     <script src="{{ asset('assets/vendors/bootstrapvalidator/js/bootstrapValidator.min.js') }}" type="text/javascript"></script>
 
 
-    <script src="{{ asset('assets/js/pages/addproductos.js') }}"></script>
+   
 
 
      <!-- Arbol de categorias -->
@@ -435,6 +435,132 @@ Nuevo Producto
         });
 
         });
+
+    </script>
+
+
+    <script type="text/javascript">
+        
+        
+
+$("#productosForm").bootstrapValidator({
+    fields: {
+        nombre_producto: {
+            validators: {
+                notEmpty: {
+                    message: 'Nombre de Producto es Requerido'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        referencia_producto: {
+            validators: {
+                notEmpty: {
+                    message: 'The referencia_producto name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        referencia_producto_sap: {
+            validators: {
+                notEmpty: {
+                    message: 'The referencia_producto_sap name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        descripcion_corta: {
+            validators: {
+                notEmpty: {
+                    message: 'descripcion_corta is required and cannot be empty'
+                }
+            },
+            minlength: 20
+        },
+        descripcion_larga: {
+            validators: {
+                notEmpty: {
+                    message: 'descripcion_corta is required and cannot be empty'
+                }
+            },
+            minlength: 20
+        },
+        seo_titulo: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_titulo name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        seo_descripcion: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_descripcion name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        seo_url: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_url name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+
+        id_categoria_default: {
+            validators: {
+                notEmpty: {
+                    message: 'Please select a id_categoria_default'
+                }
+            }
+        },
+
+        id_marca: {
+            validators:{
+                notEmpty:{
+                    message: 'You must select a id_marca'
+                }
+            }
+        }
+    }
+});
+
+
+
+$('.finish').click(function () {
+    var $validator = $('#productosForm').data('bootstrapValidator').validate();
+    if ($validator.isValid()) {
+        document.getElementById("productosForm").submit();
+    }
+
+});
+// $('#activate').on('ifChanged', function(event){
+//     $('#commentForm').bootstrapValidator('revalidateField', $('#activate'));
+// });
+$('#productosForm').keypress(
+    function(event){
+        if (event.which == '13') {
+            event.preventDefault();
+        }
+    });
+
+
+
+    $(document).ready(function(){
+        $('.select2').select2({
+            placeholder: "select",
+            theme:"bootstrap"
+        });
+    })
 
     </script>
 
--- resources/views/admin/productos/edit.blade.php
+++ resources/views/admin/productos/edit.blade.php
@@ -463,7 +463,7 @@ Productos
     <script src="{{ asset('assets/vendors/bootstrapvalidator/js/bootstrapValidator.min.js') }}" type="text/javascript"></script>
 
 
-    <script src="{{ asset('assets/js/pages/addproductos.js') }}"></script>
+    
 
 
 
@@ -483,5 +483,185 @@ Productos
 
 
 
+    <script type="text/javascript">
+        
+
+          function verificarCategorias (){
+
+              $(".node-checked").each(function(){
+                    alert($(this).text())
+                });
+              
+        }
+
+        $(document).ready(function(){
+
+        defaultData=$("#tree").val();
+
+        var $checkableTree = $('#treeview-checkable').treeview({
+            data: defaultData,
+            showIcon: false,
+            showCheckbox: true,
+            onNodeChecked: function(event, node) {
+                $('#checkable-output').prepend('<p>' + node.text + ' was checked</p>');
+
+
+                $(".node-checked").each(function(){
+                    alert($(this).text())
+                });
+
+            },
+            onNodeUnchecked: function(event, node) {
+                $('#checkable-output').prepend('<p>' + node.text + ' was unchecked</p>');
+
+              $(".node-checked").each(function(){
+                    alert($(this).text())
+                });
+
+               
+
+            }
+        });
+
+        });
+
+    </script>
+
+
+    <script type="text/javascript">
+        
+        
+
+$("#productosForm").bootstrapValidator({
+    fields: {
+        nombre_producto: {
+            validators: {
+                notEmpty: {
+                    message: 'Nombre de Producto es Requerido'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        referencia_producto: {
+            validators: {
+                notEmpty: {
+                    message: 'The referencia_producto name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        referencia_producto_sap: {
+            validators: {
+                notEmpty: {
+                    message: 'The referencia_producto_sap name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        descripcion_corta: {
+            validators: {
+                notEmpty: {
+                    message: 'descripcion_corta is required and cannot be empty'
+                }
+            },
+            minlength: 20
+        },
+        descripcion_larga: {
+            validators: {
+                notEmpty: {
+                    message: 'descripcion_corta is required and cannot be empty'
+                }
+            },
+            minlength: 20
+        },
+        seo_titulo: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_titulo name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        seo_descripcion: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_descripcion name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+        seo_url: {
+            validators: {
+                notEmpty: {
+                    message: 'The seo_url name is required'
+                }
+            },
+            required: true,
+            minlength: 3
+        },
+
+        id_categoria_default: {
+            validators: {
+                notEmpty: {
+                    message: 'Please select a id_categoria_default'
+                }
+            }
+        },
+
+        id_marca: {
+            validators:{
+                notEmpty:{
+                    message: 'You must select a id_marca'
+                }
+            }
+        }
+    }
+});
+
+
+
+$('.finish').click(function () {
+    var $validator = $('#productosForm').data('bootstrapValidator').validate();
+    if ($validator.isValid()) {
+        document.getElementById("productosForm").submit();
+    }
+
+});
+// $('#activate').on('ifChanged', function(event){
+//     $('#commentForm').bootstrapValidator('revalidateField', $('#activate'));
+// });
+$('#productosForm').keypress(
+    function(event){
+        if (event.which == '13') {
+            event.preventDefault();
+        }
+    });
+
+
+
+    $(document).ready(function(){
+        $('.select2').select2({
+            placeholder: "select",
+            theme:"bootstrap"
+        });
+    })
+
+
+</script>
+
+
     
 @stop
+
+
+
+
+
+
+
+
