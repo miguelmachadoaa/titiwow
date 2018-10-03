f07e02804be7c5730b5a242e91dbabc32f960245
Orangel Barrera <obarrerafranco@gmail.com>
2018-10-03 18:12:57 -0500

    Se crea tabla de empresas

---
+++ database/migrations/2018_10_03_213007_create_alp_empresas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlpEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alp_empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_empresa');
+            $table->text('descripcion_empresa')->nullable();
+            $table->integer('estado_registro')->default(1);
+            $table->integer('id_user');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alp_empresas');
+    }
+}
