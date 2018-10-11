0d38325ab778dd9de23cdde0557939ee94f6a22a
Orangel Barrera <obarrerafranco@gmail.com>
2018-10-11 10:03:19 -0500

    Se agrega conteo de productos en el front

--- resources/views/frontend/list.blade.php
+++ resources/views/frontend/list.blade.php
@@ -115,7 +115,10 @@ Productos
         @endif
         </div>
         <div class="row">
-            <div class="col-md-12 text-right">
+        <div class="col-md-3 text-left align-bottom">Productos {{($productos->currentpage()-1)*$productos->perpage()+1}} a {{$productos->currentpage()*$productos->perpage()}}
+            de  {{$productos->total()}}
+        </div>
+            <div class="col-md-9 text-right">
                 {{ $productos->links() }}
             </div>
         </div>
