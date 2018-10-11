18055752a408e175e1a0395f598d7285fd52a215
miguelmachadoaa <miguelmachadoaa@gmail.com>
2018-10-11 18:28:01 -0400

    actualizacion de fotos productos

---
+++ update.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+	<title>	</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "alpina");
+
+/* comprobar la conexión */
+if ($mysqli->connect_errno) {
+    printf("Falló la conexión: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+/* Crear una tabla que no devuelve un conjunto de resultados */
+
+$id=$_POST['id'];
+
+//echo "update alp_productos set imagen_producto = 'defult.png' where id='".$id."'";
+
+
+$resultado = $mysqli->query("update alp_productos set imagen_producto = 'default.png' where id='".$id."'");
+/* Consultas de selección que devuelven un conjunto de resultados */
+
+return $resultado;
+
+?>
