f83437bade3b1290cab4d47c1d68b7af32057ed1
Orangel Barrera <obarrerafranco@gmail.com>
2018-10-27 11:48:13 -0500

    Se modifica nombre area cliente -> Mi perfil

--- resources/views/frontend/clientes/index.blade.php
+++ resources/views/frontend/clientes/index.blade.php
@@ -62,7 +62,7 @@ Area clientes
                 </li>
                 <li class="hidden-xs">
                     <i class="livicon icon3" data-name="angle-double-right" data-size="18" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
-                    <a href="{{ url('clientes') }}">Area Cliente </a>
+                    <a href="{{ url('clientes') }}">Mi Perfil </a>
                 </li>
 
                 
