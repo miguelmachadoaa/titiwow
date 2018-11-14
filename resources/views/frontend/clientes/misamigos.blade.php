41e28107fef82f017f8b1ebe16664100dfa6949c
Orangel Barrera <obarrerafranco@gmail.com>
2018-11-13 19:33:10 -0500

    Ajuste Visual amigos

--- resources/views/frontend/clientes/misamigos.blade.php
+++ resources/views/frontend/clientes/misamigos.blade.php
@@ -142,71 +142,65 @@ Mis Amigos
         </div>
 
         <hr>
-
+        <div class="row">
+            <div class="col-md-12">  <h3>   Mis  Amigos Registrados  </h3> </div>
+        </div>
         <div class="row " id="table_amigos">
+            @if(!$referidos->isEmpty())
+                <div class="form-group col-sm-10 col-sm-offset-1 table-responsive">
+                    <table class="table table-responsive" id="tbAmigos">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Email</th>
+                                <th>Puntos</th>
+                                <th>Creado</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-            <div class="col-sm-8">  <h3>   Mis  Amigos Registrados  </h3> </div>
+                        @foreach($referidos as $referido)
 
-        @if(!$referidos->isEmpty())
+                                <tr>
+                                    <td>
+                                        {{ $referido->first_name }}
+                                    </td>
+                                    <td>
+                                        {{ $referido->last_name }}
+                                    </td>
+                                    <td>
+                                        {{ $referido->email }}
+                                    </td>
+                                    <td>
+                                        {{ $referido->puntos }}
+                                    </td>
+                                    <td>
+                                        {{ $referido->created_at }}
+                                    </td>
 
-        
-        <div class="form-group col-sm-10 col-sm-offset-1 table-responsive">
-             <table class="table table-responsive" id="tbAmigos">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        <th>Email</th>
-                        <th>Puntos</th>
-                        <th>Creado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
+                                    <td>    
 
-            @foreach($referidos as $referido)
+                                            <a class="btn btn-xs" href="{{ url('clientes/'.$referido->id_user_client.'/compras') }}">
+                                                <i class="livicon" data-name="eye" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="Ver Compras"></i>
+                                            </a>
 
-                    <tr>
-                        <td>
-                            {{ $referido->first_name }}
-                        </td>
-                        <td>
-                            {{ $referido->last_name }}
-                        </td>
-                        <td>
-                            {{ $referido->email }}
-                        </td>
-                        <td>
-                            {{ $referido->puntos }}
-                        </td>
-                        <td>
-                            {{ $referido->created_at }}
-                        </td>
-
-                        <td>    
-
-                                  <a class="btn btn-xs" href="{{ url('clientes/'.$referido->id_user_client.'/compras') }}">
-                                    <i class="livicon" data-name="eye" data-size="18" data-loop="true" data-c="#428BCA" data-hc="#428BCA" title="Ver Compras"></i>
-                                 </a>
-
-                                 <button class="btn btn-xs btn-danger eliminarAmigo" data-id="{{ $referido->id_user_client }}"> <i class="fa fa-trash"> </i>  </button>
+                                            <button class="btn btn-xs btn-danger eliminarAmigo" data-id="{{ $referido->id_user_client }}"> <i class="fa fa-trash"> </i>  </button>
 
 
-                        </td>
-                    </tr>
-                
-            @endforeach
-            </tbody>
-             </table>
-
-             </div>
-
-        
+                                    </td>
+                                </tr>
+                            
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             @else
-            <div class="alert alert-danger">
-                <strong>Lo Sentimos!</strong> No Existen Referidos aún.
-            </div>
-        @endif
+                <div class="alert alert-danger">
+                    <strong>Lo Sentimos!</strong> No Existen Referidos aún.
+                </div>
+            @endif
         </div>
         
     </div>
