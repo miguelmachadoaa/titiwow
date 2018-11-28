fd776c0c79b701958cec9c0a0053d88f5300ed24
miguelmachadoaa <miguelmachadoaa@gmail.com>
2018-11-28 00:37:01 -0400

    Reporte de ventas por usuario y rango de fechas

---
+++ resources/views/admin/reportes/ventas.blade.php
@@ -0,0 +1,164 @@
+@extends('admin/layouts/default')
+
+{{-- Web site Title --}}
+@section('title')
+Reporte de Ventas 
+@parent
+@stop
+
+@section('header_styles')
+    <link href="{{ asset('assets/vendors/pickadate/css/default.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/vendors/pickadate/css/default.date.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/vendors/pickadate/css/default.time.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/vendors/airDatepicker/css/datepicker.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/vendors/flatpickrCalendar/css/flatpickr.min.css') }}" rel="stylesheet"
+          type="text/css"/>
+    <link href="{{ asset('assets/css/pages/adv_date_pickers.css') }}" rel="stylesheet" type="text/css"/>
+@stop
+
+
+
+
+{{-- Content --}}
+@section('content')
+<section class="content-header">
+    <h1>Reporte de Ventas por cliente </h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{ route('admin.dashboard') }}">
+                <i class="livicon" data-name="home" data-size="14" data-color="#000"></i>
+               Inicio
+            </a>
+        </li>
+        <li><a href="#"> Reportes </a></li>
+        <li class="active">Ventas por clientes</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-primary ">
+                <div class="panel-heading clearfix">
+                    <h4 class="panel-title pull-left"> <i class="livicon" data-name="users" data-size="16" data-loop="true" data-c="#fff" data-hc="white"></i>
+                       Reporte ventas por clientes 
+                    </h4>
+                </div>
+                <br />
+                <div class="panel-body">
+
+                    <form class="form-horizontal" role="form" method="post" action="{{ secure_url('admin/reportes/exportventas') }}">
+
+                        {{ csrf_field() }}
+
+                <div class="row">   
+                    <div class="form-group col-sm-12 ">
+                        <label for="select21" class="col-md-2 control-label text-right">
+                        Clientes                                               
+                        </label>
+                        <div class="col-md-10">   
+                            <select id="clientes" name="clientes" class="form-control ">
+                                <option value="">Seleccione</option>
+                                 @foreach($clientes as $cliente)
+                                 
+                                    <option  value="{{ $cliente->id }}">{{ $cliente->first_name.' '.$cliente->last_name }}</option>
+
+                                 @endforeach
+                                
+                            </select>
+                        </div>           
+                    </div>
+
+
+                    <div class="form-group">
+
+                                <label class="col-md-2 control-label text-right">Desde - Hasta:</label>
+
+                        <div class="row">
+                                <div class="col-sm-5 pad-0-res mt-5">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="livicon" data-name="calendar" data-size="16" data-c="#555555"
+                                           data-hc="#555555" data-loop="true"></i>
+                                    </div>
+                                    <input class="form-control" id="desde" name="desde" placeholder="Desde">
+                                </div>
+                            </div>
+                                <div class="col-sm-5 pad-0-res mt-5">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="livicon" data-name="calendar" data-size="16" data-c="#555555"
+                                           data-hc="#555555" data-loop="true"></i>
+                                    </div>
+                                    <input class="form-control" id="hasta" name="hasta" placeholder="Hasta">
+                                </div>
+                            </div>
+                        </div>
+                            <!-- /.input group -->
+                    </div>
+
+
+
+                    
+
+                    <div class="form-group">
+                            <div class="col-sm-offset-4 col-sm-8">
+                                <button type="submit" class="btn btn-md btn-primary">  Descargar ventas en Excel  </button>
+                                
+                                <a href="{{ secure_url('admin/reportes/exportventas') }}" class="btn btn-md btn-primary" id="generar_report" target="_blank">
+                                    Descargar productos en Excel
+                                </a>
+
+                                <a class="btn btn-md btn-danger" href="{{ route('admin.sedes.index') }}">
+                                    Cancelar
+                                </a>
+                            
+                            </div>
+                        </div>
+
+                </div>
+
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>    <!-- row-->
+</section>
+
+
+
+
+@stop
+
+
+
+{{-- page level scripts --}}
+@section('footer_scripts')
+    <!-- begining of page level js -->
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.date.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.time.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/flatpickrCalendar/js/flatpickr.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/airDatepicker/js/datepicker.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/airDatepicker/js/datepicker.en.js') }}" type="text/javascript"></script>
+   
+   <script type="text/javascript">
+       
+
+    var desde = flatpickr("#desde");
+    
+    var hasta = flatpickr("#hasta");
+
+    desde.set("onChange", function (d) {
+        hasta.set("minDate", d.fp_incr(1)); //increment by one day
+    });
+    hasta.set("onChange", function (d) {
+        desde.set("maxDate", d);
+    });
+
+
+
+   </script>
+
+@stop
