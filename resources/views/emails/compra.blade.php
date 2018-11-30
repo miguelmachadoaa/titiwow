8941ac379b78a5c4faaff73869b6ee853428b100
miguelmachadoaa <miguelmachadoaa@gmail.com>
2018-11-30 15:44:54 -0400

    ajuste de campos en notificacion de copra a sac

--- resources/views/emails/compra.blade.php
+++ resources/views/emails/compra.blade.php
@@ -3,12 +3,23 @@ Gracias por su compra {{ $compra->first_name.' '.$compra->last_name }}
 
 Hemos registrado una compra {{ $compra->referencia }},  Ha seleccionado enviar el pedido con <b>{{ $compra->nombre_forma_envios }}</b> y será entregado {{ $fecha_entrega }}. 
 
+Datos de la compra
+
+<p><b>IdPedido: </b>{{ $compra->id }}</p>
+<p><b>IdUsuario Masterfile: </b>{{ $compra->cod_oracle_cliente }}</p>
+<p><b>Cedula: </b>{{ $compra->doc_cliente }}</p>
+<p><b>Valor Pagado: </b>{{ $compra->monto_total }}</p>
+<p><b>Valor Iva: </b>{{ 0}}</p>
+<p><b>Fecha de Entrega: </b>{{ $compra->fecha_envio }}</p>
+
 
 
 <h3>Detalle de compra</h3>
 
 <table width="100%">
 	<tr>
+		<th>Sku</th>
+		<th>idProducto</th>
 		<th>Producto</th>
          <th>Precio</th>
          <th>Cantidad</th>
@@ -18,6 +29,8 @@ Hemos registrado una compra {{ $compra->referencia }},  Ha seleccionado enviar e
 	@foreach($detalles as $row)
 
 		<tr>
+		<td>{{$row->referencia_producto}}</td>
+		<td>{{$row->id_producto}}</td>
 		<td>{{$row->nombre_producto}}</td>
         <td>{{number_format($row->precio_unitario,0,",",".")}}</td>
         <td> {{ $row->cantidad }} </td>
