f93dd2d08f665c99b21eb700ecbc74beeef3f321
Orangel Barrera <obarrerafranco@gmail.com>
2018-12-01 10:51:05 -0500

    Migration y seeder de abreviacion direccion

--- database/seeds/EstrucAddressTableSeeder.php
+++ database/seeds/EstrucAddressTableSeeder.php
@@ -13,17 +13,17 @@ class EstrucAddressTableSeeder extends Seeder
     public function run()
     {
         DB::table('alp_direcciones_estructura')->insert([
-            ['nombre_estructura' => 'Calle','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Carrera','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Avenida','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Avenida Carrera','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Avenida Calle','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Circular','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Circunvalar','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Diagonal','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Manzana','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Transversal','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre_estructura' => 'Via','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
+            ['nombre_estructura' => 'Calle','abrevia_estructura' => 'CL','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Carrera','abrevia_estructura' => 'CR','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Avenida','abrevia_estructura' => 'AV','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Avenida Carrera','abrevia_estructura' => 'AK','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Avenida Calle','abrevia_estructura' => 'AC','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Circular','abrevia_estructura' => 'CIR','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Circunvalar','abrevia_estructura' => 'CV','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Diagonal','abrevia_estructura' => 'DG','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Manzana','abrevia_estructura' => 'MZ','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Transversal','abrevia_estructura' => 'TV','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre_estructura' => 'Via','abrevia_estructura' => 'V','id_user' => 1,'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
         ]);
     }
 }
