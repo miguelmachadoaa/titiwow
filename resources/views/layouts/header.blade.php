eb24a4c7bda1b42cf561255b0741c5021104e027
Orangel Barrera <obarrerafranco@gmail.com>
2018-12-13 17:11:40 -0500

    Se añade tagmanager de cliente

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -34,18 +34,34 @@
     <!--page level css-->
     @yield('header_styles')
     <!--end of page level css-->
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129370910-1"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        @if (App::environment('local')) 
 
-    gtag('config', 'UA-129370910-1');
-    </script>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129370910-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-129370910-1');
+            </script>
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PG9RTMH');</script>
+            <!-- End Google Tag Manager -->
+        @endif
 </head>
 
 <body>
+    @if (App::environment('local')) 
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG9RTMH"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    @endif
     <!-- Header Start -->
     <header> 
         <!-- Icon Section Start -->
