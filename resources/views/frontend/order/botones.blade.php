348c1f5520306b77d25464c0486edab557d96358
miguelmachadoaa <miguelmachadoaa@gmail.com>
2018-12-14 11:56:24 -0400

    ajsute descuadre respuesta botones

--- resources/views/frontend/order/botones.blade.php
+++ resources/views/frontend/order/botones.blade.php
@@ -1,11 +1,11 @@
  
 @if(isset($producto->id))
 
-<a class="btn btn-md btn-vermas" href="{{ route('producto', [$producto->slug]) }}" style="margin-bottom:5px;">Ver <i class="fa fa-plus" aria-hidden="true"></i></a>
+<a class="btn btn-md btn-vermas" href="{{ route('producto', [$producto->slug]) }}" style="">Ver <i class="fa fa-plus" aria-hidden="true"></i></a>
 
    @if(isset($cart[$producto->slug]))
 
-          <div class="row" style="margin-bottom:5px;">
+          <div class="row" style="margin-top:5px;">
             <div class="col-sm-10 col-sm-offset-1">
               <div class="input-group">
                 <span class="input-group-btn">
@@ -23,7 +23,14 @@
 
     @else
 
-        <a data-slug="{{ $producto->slug }}" data-price="{{ intval($producto->precio_base) }}" data-id="{{ $producto->id }}" data-name="{{ $producto->nombre_producto }}" data-imagen="{{ secure_url('/').'/uploads/productos/'.$producto->imagen_producto }}" class="btn btn-md btn-cart addtocart" href="{{secure_url('cart/addtocart', [$producto->slug])}}" alt="Agregar al Carrito"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></a>
+        <a 
+        data-slug="{{ $producto->slug }}" 
+        data-price="{{ intval($producto->precio_base) }}" 
+        data-id="{{ $producto->id }}" 
+        data-name="{{ $producto->nombre_producto }}" 
+        data-imagen="{{ secure_url('/').'/uploads/productos/'.$producto->imagen_producto }}" 
+        class="btn btn-md btn-cart addtocart" 
+        href="{{secure_url('cart/addtocart', [$producto->slug])}}" alt="Agregar al Carrito"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></a>
 
 
     @endif
