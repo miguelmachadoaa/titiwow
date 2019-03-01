8a5cc3f43511473bc58d77773b81e504d4ddee54
Orangel Barrera <obarrerafranco@gmail.com>
2019-03-01 18:30:23 -0500

    Ajuste envio de email

--- app/Console/Commands/VerificarPagos.php
+++ app/Console/Commands/VerificarPagos.php
@@ -4,6 +4,13 @@ namespace App\Console\Commands;
 
 use App\User;
 use App\Models\AlpConfiguracion;
+use App\Models\AlpDirecciones;
+use App\Models\AlpFeriados;
+use App\Models\AlpFormaCiudad;
+
+
+
+
 
 use App\Models\AlpPagos;
 use App\Models\AlpEnvios;
@@ -75,7 +82,6 @@ class VerificarPagos extends Command
 
         MP::setCredenciales($configuracion->id_mercadopago, $configuracion->key_mercadopago);
 
-
         foreach ($ordenes as $ord) {
 
           $orden=AlpOrdenes::where('id', $ord->id)->first();
@@ -84,6 +90,14 @@ class VerificarPagos extends Command
 
           $preference = MP::get("/v1/payments/search?external_reference=".$ord->referencia);
 
+          $preference=array(
+            'response' => array(
+              'results' => array(
+                0 => array(
+                  'status' => 'approved' )
+                ) 
+              ) 
+          );
 
           if (isset($preference['response']['results'][0])) {
 
@@ -111,19 +125,66 @@ class VerificarPagos extends Command
             if ( $preference['response']['results'][0]['status']=='approved' ) 
               {
 
+                  $direccion=AlpDirecciones::where('id', $ord->id_address)->first();
 
-                  $envio=AlpEnvios::where('id_orden', $orden->id)->first();
+                $feriados=AlpFeriados::feriados();
+
+                $ciudad_forma=AlpFormaCiudad::where('id_forma', $ord->id_forma_envio)->where('id_ciudad', $direccion->city_id)->first();
 
 
-                    $data_update = array(
-                      'estatus' =>1, 
-                      'estatus_pago' =>2,
-                       );
+                $date = Carbon::now();
 
-                     $orden->update($data_update);
+                $hora=$date->format('hi');
+
+                $hora_base=str_replace(':', '', $ciudad_forma->hora);
+
+                if (intval($hora)>intval($hora_base)) {
+
+                  $ciudad_forma->dias=$ciudad_forma->dias+1;
+
+                }
+
+                for ($i=0; $i <=$ciudad_forma->dias ; $i++) { 
+
+                  $date2 = Carbon::now();
+
+                  $date2->addDays($i);
+
+                  if ($date2->isWeekend()) {
+
+                    $ciudad_forma->dias=$ciudad_forma->dias+1;
+                  
+                  }else{
+
+                    if (isset($feriados[$date2->format('Y-m-d')])) {
+
+                        $ciudad_forma->dias=$ciudad_forma->dias+1;
+                     
+                    }
+
+                  }
+
+                  
+                }
+
+                $fecha_entrega=$date->addDays($ciudad_forma->dias)->format('d-m-Y');
+
+
+                $data_envio = array(
+                  'id_orden' => $ord->id, 
+                  'fecha_envio' => $date->addDays($ciudad_forma->dias)->format('Y-m-d'),
+                  'estatus' => 1, 
+                  'id_user' =>1                   
+
+                );
+
+                $envio=AlpEnvios::create($data_envio);
+
+
+                 
 
                      $data_history = array(
-                          'id_orden' => $orden->id, 
+                          'id_orden' => $ord->id, 
                          'id_status' => '1', 
                           'notas' => 'Notificacion Mercadopago', 
                          'id_user' => 1
@@ -132,42 +193,31 @@ class VerificarPagos extends Command
                       $history=AlpOrdenesHistory::create($data_history);
 
 
-            $compra =  DB::table('alp_ordenes')->select('alp_ordenes.*','users.first_name as first_name','users.last_name as last_name' ,'users.email as email','alp_formas_envios.nombre_forma_envios as nombre_forma_envios','alp_formas_envios.descripcion_forma_envios as descripcion_forma_envios','alp_formas_pagos.nombre_forma_pago as nombre_forma_pago','alp_formas_pagos.descripcion_forma_pago as descripcion_forma_pago','alp_clientes.cod_oracle_cliente as cod_oracle_cliente','alp_clientes.doc_cliente as doc_cliente')
-            ->join('users','alp_ordenes.id_cliente' , '=', 'users.id')
-            ->join('alp_clientes','alp_ordenes.id_cliente' , '=', 'alp_clientes.id_user_client')
-            ->join('alp_formas_envios','alp_ordenes.id_forma_envio' , '=', 'alp_formas_envios.id')
-            ->join('alp_formas_pagos','alp_ordenes.id_forma_pago' , '=', 'alp_formas_pagos.id')
-            ->where('alp_ordenes.id', $orden->id)->first();
-
-            $detalles =  DB::table('alp_ordenes_detalle')->select('alp_ordenes_detalle.*','alp_productos.nombre_producto as nombre_producto','alp_productos.referencia_producto as referencia_producto' ,'alp_productos.referencia_producto_sap as referencia_producto_sap' ,'alp_productos.imagen_producto as imagen_producto','alp_productos.slug as slug')
-              ->join('alp_productos','alp_ordenes_detalle.id_producto' , '=', 'alp_productos.id')
-              ->where('alp_ordenes_detalle.id_orden', $orden->id)->get();
-
-            
-
-              Mail::to($user_cliente->email)->send(new \App\Mail\CompraRealizada($compra, $detalles, $envio->fecha_envio));
-
-              Mail::to($configuracion->correo_sac)->send(new \App\Mail\CompraSac($compra, $detalles, $envio->fecha_envio));
-               
-            }
-
-            if ( $preference['response']['results'][0]['status']=='in_process' || $preference['response']['results'][0]['status']=='pending' ) 
-              {
-                  /*  $data_update = array(
-                      'estatus' =>8, 
-                      'estatus_pago' =>4,
+                      $data_update = array(
+                      'estatus' =>1, 
+                      'estatus_pago' =>2,
                        );
 
                      $orden->update($data_update);
 
-                      $data_history = array(
-                          'id_orden' => $orden->id, 
-                         'id_status' => '8', 
-                          'notas' => 'Notificacion Mercadopago', 
-                         'id_user' => 1
-                      );
 
-                        $history=AlpOrdenesHistory::create($data_history);*/
+
+               $compra =  DB::table('alp_ordenes')->select('alp_ordenes.*','users.first_name as first_name','users.last_name as last_name' ,'users.email as email','alp_formas_envios.nombre_forma_envios as nombre_forma_envios','alp_formas_envios.descripcion_forma_envios as descripcion_forma_envios','alp_formas_pagos.nombre_forma_pago as nombre_forma_pago','alp_formas_pagos.descripcion_forma_pago as descripcion_forma_pago','alp_clientes.cod_oracle_cliente as cod_oracle_cliente','alp_clientes.doc_cliente as doc_cliente')
+                ->join('users','alp_ordenes.id_cliente' , '=', 'users.id')
+                ->join('alp_clientes','alp_ordenes.id_cliente' , '=', 'alp_clientes.id_user_client')
+               ->join('alp_formas_envios','alp_ordenes.id_forma_envio' , '=', 'alp_formas_envios.id')
+               ->join('alp_formas_pagos','alp_ordenes.id_forma_pago' , '=', 'alp_formas_pagos.id')
+               ->where('alp_ordenes.id', $orden->id)->first();
+
+                $detalles =  DB::table('alp_ordenes_detalle')->select('alp_ordenes_detalle.*','alp_productos.nombre_producto as nombre_producto','alp_productos.referencia_producto as referencia_producto' ,'alp_productos.referencia_producto_sap as referencia_producto_sap' ,'alp_productos.imagen_producto as imagen_producto','alp_productos.slug as slug')
+                  ->join('alp_productos','alp_ordenes_detalle.id_producto' , '=', 'alp_productos.id')
+                  ->where('alp_ordenes_detalle.id_orden', $orden->id)->get();
+
+            
+
+                  Mail::to($user_cliente->email)->send(new \App\Mail\CompraRealizada($compra, $detalles, $fecha_entrega));
+
+                 Mail::to($configuracion->correo_sac)->send(new \App\Mail\CompraSac($compra, $detalles, $fecha_entrega));
                
             }
 
@@ -176,35 +226,33 @@ class VerificarPagos extends Command
           if (isset($preference['response']['results'][0])) {
             # code...
 
-            $res = array('response' => $preference['response']['results'][0] );
+              $res = array('response' => $preference['response']['results'][0] );
 
-            if ( $preference['response']['results'][0]['status']=='in_process' || $preference['response']['results'][0]['status']=='pending' ) 
-              {
+              if ( $preference['response']['results'][0]['status']=='in_process' || $preference['response']['results'][0]['status']=='pending' ) 
+                {
 
-              }else{
-
-
-                $data_pago = array(
-              'id_orden' => $orden->id, 
-              'id_forma_pago' => $orden->id_forma_pago, 
-              'id_estatus_pago' => 4, 
-              'monto_pago' => $orden->monto_total, 
-              'json' => json_encode($res), 
-              'id_user' => '0' 
-            );
+                }else{
 
 
-           AlpPagos::create($data_pago);
-              }
+                  $data_pago = array(
+                'id_orden' => $orden->id, 
+                'id_forma_pago' => $orden->id_forma_pago, 
+                'id_estatus_pago' => 4, 
+                'monto_pago' => $orden->monto_total, 
+                'json' => json_encode($res), 
+                'id_user' => '0' 
+              );
 
-            
+             AlpPagos::create($data_pago);
+                }
 
-         }
+           }
 
 
-        }
+        }//endforeach ordenes
 
 
 
-    }
-}
+    }//endhadle
+
+}//endclass
