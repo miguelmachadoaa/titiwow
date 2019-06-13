d12e223a08d4a66f946efd791446475b018854dc
miguelmachadoaa <miguelmachadoaa@gmail.com>
2019-06-13 18:19:17 -0500

    MEJORA: Colocar la cantidad en el select del carritpo

--- resources/views/frontend/includes/newcart.blade.php
+++ resources/views/frontend/includes/newcart.blade.php
@@ -41,16 +41,13 @@
                                             <span class="select">
                                                 <select class="cartselect" data-id="{{ $row->id }}" data-slug="{{ $row->slug }}" id="cd-product-{{ $row->id }}" name="quantity">
                                                     <option value="{{ $row->cantidad }}">{{ $row->cantidad }}</option>
-                                                    <option value="1">1</option>
-                                                    <option value="2">2</option>
-                                                    <option value="3">3</option>
-                                                    <option value="4">4</option>
-                                                    <option value="5">5</option>
-                                                    <option value="6">6</option>
-                                                    <option value="7">7</option>
-                                                    <option value="8">8</option>
-                                                    <option value="9">9</option>
-                                                    <option value="10">10</option>
+
+                                                    @for ($i = 1; $i <= $configuracion->maximo_productos; $i++)
+                                                        The current value is {{ $i }}
+                                                        <option value="{{ $i }}">{{ $i }}</option>
+                                                    @endfor
+
+                                                    
                                                 </select>
                                             </span>
                                     </div>
