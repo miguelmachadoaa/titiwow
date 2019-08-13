4d161ca638943fd4a0d0da31d05a0ceaef7e4746
miguelmachadoaa <miguelmachadoaa@gmail.com>
2019-08-13 11:16:06 -0500

    ERROR: Pantalla negra al estar duplicado el convenio

--- app/Http/Requests/EmpresaRequest.php
+++ app/Http/Requests/EmpresaRequest.php
@@ -24,12 +24,37 @@ class EmpresaRequest extends FormRequest {
 	 */
 	public function rules()
 	{
-		return [
-            'nombre_empresa' => 'required|min:3',
-            'descripcion_empresa' => 'required|min:3',
-			'descuento_empresa' => 'required',
-			'dominio' => 'required',
-		];
+
+
+		switch ($this->method()) {
+            case 'GET':
+            case 'DELETE': {
+                return [];
+            }
+            case 'POST': {
+                return [
+                     'nombre_empresa' => 'required|min:3',
+		            'descripcion_empresa' => 'required|min:3',
+					'descuento_empresa' => 'required',
+					'convenio' => 'required|unique:alp_empresas,convenio',
+					'dominio' => 'required'                  
+                ];
+            }
+            case 'PUT':
+            case 'PATCH': {
+                return [
+                   'nombre_empresa' => 'required|min:3',
+		            'descripcion_empresa' => 'required|min:3',
+					'descuento_empresa' => 'required',
+					'convenio' => 'required|unique:alp_empresas,convenio,'. $this->id_empresa,
+					'dominio' => 'required'
+                ];
+            }
+            default:
+                break;
+        }
+
+		
 	}
 
 }
