09f64680cd42ae4b45b18331d9f2ba56b1ae0224
miguelmachadoaa <miguelmachadoaa@gmail.com>
2020-05-12 08:51:24 -0500

    MEJORA menu barrios

--- resources/views/admin/layouts/_left_menu.blade.php
+++ resources/views/admin/layouts/_left_menu.blade.php
@@ -319,7 +319,16 @@
         </ul>
     @endif
 
-   
+    @if (Sentinel::getUser()->hasAnyAccess(['barrios.*']))
+        <ul class="sub-menu">
+            <li {!! (Request::is('admin/barrios*') ? 'class="active"' : '') !!}>
+                <a href="{!! route('admin.barrios.index') !!}">
+                    <i class="fa fa-angle-double-right"></i>
+                    Barrios
+                </a>
+            </li> 
+        </ul>
+    @endif
 
 
 
