98d95cc3e67de3f8f49c9da2bf3b4866e634a5c8
miguelmachadoaa <miguelmachadoaa@gmail.com>
2020-08-14 10:40:33 -0500

    Ajuste de llamada a compra mas despues de recibido

--- app/Http/Controllers/Admin/AlpOrdenesController.php
+++ app/Http/Controllers/Admin/AlpOrdenesController.php
@@ -2552,11 +2552,7 @@ public function compramasupdate()
 
         if ($orden->id) {
 
-
-          //$user_cliente=Users::where('id', $orden->id_cliente)->first();
-
           $texto="La orden ".$orden->id.", el pago ha sido notificado y espera por el proceso de aprobación!";
-
 
           Mail::to($configuracion->correo_cedi)->send(new \App\Mail\NotificacionOrden($orden->id, $texto));
 
@@ -2731,95 +2727,6 @@ public function compramasupdate()
             }
 
           Mail::to($configuracion->correo_cedi)->send(new \App\Mail\CompraSac($orden, $detalles, $fecha_entrega));
-
-
-
-          $orden=AlpOrdenes::where('id', $orden->id)->first();
-
-                 $detalles = AlpDetalles::select('alp_ordenes_detalle.*','alp_productos.nombre_producto as nombre_producto','alp_productos.imagen_producto as imagen_producto','alp_productos.referencia_producto as referencia_producto')
-                  ->join('alp_productos', 'alp_ordenes_detalle.id_producto', '=', 'alp_productos.id')
-                  ->where('alp_ordenes_detalle.id_orden', $orden->id)
-                  ->get();
-
-                  $productos = array();
-
-                  foreach ($detalles as $d) {
-                    
-                      $dt = array(
-                        'sku' => $d->referencia_producto, 
-                        'name' => $d->nombre_producto, 
-                        'url_img' => $d->imagen_producto, 
-                        'value' => $d->precio_unitario, 
-                        'value_prom' => $d->precio_unitario, 
-                        'quantity' => $d->cantidad
-                      );
-
-                      $productos[]=$dt;
-                  }
-
-              $cliente =  User::select('users.*','roles.name as name_role','alp_clientes.estado_masterfile as estado_masterfile','alp_clientes.estado_registro as estado_registro','alp_clientes.telefono_cliente as telefono_cliente','alp_clientes.cod_oracle_cliente as cod_oracle_cliente','alp_clientes.cod_alpinista as cod_alpinista','alp_clientes.doc_cliente as doc_cliente')
-                ->join('alp_clientes', 'users.id', '=', 'alp_clientes.id_user_client')
-                ->join('role_users', 'users.id', '=', 'role_users.user_id')
-                ->join('roles', 'role_users.role_id', '=', 'roles.id')
-                ->where('users.id', '=', $orden->id_user)->first();
-
-
-              $direccion = AlpDirecciones::select('alp_direcciones.*', 'config_cities.city_name as city_name', 'config_states.state_name as state_name','config_states.id as state_id','config_countries.country_name as country_name', 'alp_direcciones_estructura.nombre_estructura as nombre_estructura', 'alp_direcciones_estructura.id as estructura_id')
-              ->join('config_cities', 'alp_direcciones.city_id', '=', 'config_cities.id')
-              ->join('config_states', 'config_cities.state_id', '=', 'config_states.id')
-              ->join('config_countries', 'config_states.country_id', '=', 'config_countries.id')
-              ->join('alp_direcciones_estructura', 'alp_direcciones.id_estructura_address', '=', 'alp_direcciones_estructura.id')
-              ->where('alp_direcciones.id', $orden->id_address)->withTrashed()->first();
-
-
-              $dir = array(
-                'ordenId' => $orden->referencia, 
-                'ciudad' => $direccion->state_name, 
-                'telefonoCliente' => $cliente->telefono_cliente, 
-                'identificacionCliente' => $cliente->doc_cliente, 
-                'nombreCliente' => $cliente->first_name." ".$cliente->last_name, 
-                'direccionCliente' => $direccion->nombre_estructura." ".$direccion->principal_address." - ".$direccion->secundaria_address." ".$direccion->edificio_address." ".$direccion->detalle_address." ".$direccion->barrio_address, 
-                'observacionDomicilio' => "", 
-                'formaPago' => "Efectivo"
-              );
-
-              $o = array(
-                'tipoServicio' => 1, 
-                'retorno' => "false", 
-                'totalFactura' => $orden->referencia, 
-                'subTotal' => $orden->base_impuesto, 
-                'iva' => $orden->monto_impuesto, 
-                'fechaPedido' => date("Ymd", strtotime($orden->created_at)), 
-                'horaMinPedido' => "00:00", 
-                'horaMaxPedido' => "00:00", 
-                'observaciones' => "", 
-                'paradas' => $dir, 
-                'products' => $productos, 
-              );
-
-
-              $dataupdate = array(
-                'ordenId' => $orden->referencia, 
-                'status' => 'approved', 
-              );
-
-
-              //$dataraw=json_encode($o);
-              $dataraw=json_encode($dataupdate);
-
-              activity()->withProperties($dataupdate)->log('compramas dataraw ');
-
-               $configuracion = AlpConfiguracion::where('id','1')->first();
-
-           //  dd($dataraw);
-             
-             if ($orden->id_almacen==1) {
-
-                $this->sendcompramas($orden->id, 'approved');
-             }
-
-
-               
 
 
 
