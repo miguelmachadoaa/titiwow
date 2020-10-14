30ba90a5fc58acb221120029f9f0315cb1347269
miguelmachadoaa <miguelmachadoaa@gmail.com>
2020-10-14 11:07:40 -0500

    Ajuste de llamadas ibm para captura de errores

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -1864,77 +1864,165 @@ class FrontEndController extends JoshController
 
 
 
+
+
 public function makeRequest($endpoint, $jsessionid, $xml, $ignoreResult = false)
 {
+
     $url = $this->getApiUrl($endpoint, $jsessionid);
 
+
+
     echo  $url.'<br>';
+
     
+
     $xmlObj = new \SimpleXmlElement($xml);
+
+
 
     $request = $xmlObj->asXml();
 
+
+
     $curl = curl_init();
+
     curl_setopt($curl, CURLOPT_URL, $url);
+
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
     curl_setopt($curl, CURLOPT_POST, true);
+
     curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+
     curl_setopt($curl, CURLINFO_HEADER_OUT, true);
 
+
+
     $headers = array(
+
         'Content-Type: text/xml; charset=UTF-8',
+
         'Content-Length: ' . strlen($request),
+
     );
 
+
+
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
     curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
+
     curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+
+
 
     $response = @curl_exec($curl);
 
+
+
     if (false === $response) {
-        throw new Exception('CURL error: ' . curl_error($curl));
+
+        //throw new Exception('CURL error: ' . curl_error($curl));
+        //
+        return false;
+
     }
+
+
 
     curl_close($curl);
 
+
+
     if (true === $response || !trim($response)) {
-        throw new Exception('Empty response from WCA');
+
+       // throw new Exception('Empty response from WCA');
+       // 
+       $response=false;
+       
+       return false;
+
     }
 
-    $xmlResponse = simplexml_load_string($response);
+
+    if ($response==false) {
+
+      $xmlResponse = false;
+     
+    }else{
+
+      $xmlResponse = simplexml_load_string($response);
+    }
+    
+
+
 
     if (false === $ignoreResult) {
+
         if (false === isset($xmlResponse->Body->RESULT)) {
-            var_dump($xmlResponse);
-            throw new Exception('Unexpected response from WCA');
+
+           // var_dump($xmlResponse);
+
+            //throw new Exception('Unexpected response from WCA');
+            //
+            return false;
+
+        }else{
+
+          return $xmlResponse->Body->RESULT;
         }
 
-        return $xmlResponse->Body->RESULT;
+
+
+        
+
     }
 
+
+
     return $xmlResponse->Body;
+
 }
+
+
 
 public function getApiUrl($endpoint, $jsessionid)
 {
     return $endpoint . ((null === $jsessionid)
+
         ? ''
+
         : ';jsessionid=' . urlencode($jsessionid));
+
 }
+
+
 
    public function xmlToJson($xml)
   {
+
       return json_encode($xml);
+
   }
 
+
+
   public function xmlToArray($xml)
+
   {
+
       $json = $this->xmlToJson($xml);
+
       return json_decode($json, true);
+
   }
+
+
 
 
 
