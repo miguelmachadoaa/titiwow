da7143585aabf62b71d057ef5fdcdf550e73868f
miguelmachadoaa <miguelmachadoaa@gmail.com>
2020-10-28 16:15:49 -0500

    eliminar comentarios en exports

--- app/Exports/DescuentoVentasExport.php
+++ app/Exports/DescuentoVentasExport.php
@@ -75,65 +75,8 @@ class DescuentoVentasExport implements FromView
 
             $ord->total_articulos=count($detalles);
 
-
-
           }
-
-         // dd($ordenes);
-
-
-       /* $o=AlpOrdenes::select(
-          'alp_ordenes.id as id',
-          
-          'alp_ordenes.monto_descuento as monto_descuento',
-          'alp_ordenes.base_impuesto as base_impuesto',
-          'alp_ordenes.valor_impuesto as valor_impuesto',
-          'alp_ordenes.monto_impuesto as monto_impuesto',
-          'alp_ordenes.origen as origen', 
-          'alp_ordenes.referencia as referencia', 
-          'alp_ordenes.monto_total as monto_total',
-          'alp_ordenes_descuento.codigo_cupon as codigo_cupon',
-          'alp_productos.referencia_producto as referencia_producto',
-          'alp_productos.referencia_producto_sap as referencia_producto_sap',
-          DB::raw('count(alp_ordenes_detalle.cantidad) as total_articulos'),
-          DB::raw('DATE_FORMAT(alp_ordenes.created_at, "%d/%m/%Y")  as fecha'),
-          'alp_clientes.doc_cliente as doc_cliente',
-          'users.id as id_usuario', 
-          'users.first_name as first_name', 
-          'users.last_name as last_name') 
-          
-          ->join('users', 'alp_ordenes.id_cliente', '=', 'users.id')
-          ->join('alp_clientes', 'alp_ordenes.id_cliente', '=', 'alp_clientes.id_user_client')
-          ->join('alp_ordenes_detalle', 'alp_ordenes.id', '=', 'alp_ordenes_detalle.id_orden')
-          ->join('alp_productos', 'alp_ordenes_detalle.id_producto', '=', 'alp_productos.id')
-          ->leftJoin('alp_ordenes_descuento', 'alp_ordenes.id', '=', 'alp_ordenes_descuento.id_orden')
-          ->groupBy('alp_ordenes.id')
-          ->where('alp_ordenes.estatus_pago','=', '2')
-          ->where('alp_ordenes.id_forma_pago', '<>', '3')
-          
-          ->whereDate('alp_ordenes.created_at', '>=', $this->desde)
-          ->whereDate('alp_ordenes.created_at', '<=', $this->hasta);
-          //->get();
-
-          if ($this->origen==-1) {
-            # code...
-          }else{
-
-            $o->where('alp_ordenes.origen', '=', $this->origen);
-          }
-
-
-          if ($this->id_almacen==0) {
-            # code...
-          }else{
-
-            $o->where('alp_ordenes.id_almacen', '=', $this->id_almacen);
-          }
-
-
-          $ordenes=$o->get();*/
          
-
         return view('admin.exports.ventasdescuento', [
             'ventas' => $ordenes
         ]);
