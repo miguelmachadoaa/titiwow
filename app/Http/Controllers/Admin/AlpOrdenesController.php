d5fa3b19ef19e4843bcb704564ea711d1e265a51
Orangel Barrera <obarrerafranco@gmail.com>
2020-12-07 10:39:52 -0500

    Ajustes pdf ob

--- app/Http/Controllers/Admin/AlpOrdenesController.php
+++ app/Http/Controllers/Admin/AlpOrdenesController.php
@@ -4919,7 +4919,7 @@ public function sendcompramascancelar($id_orden){
 
               $j=$j+1;
 
-              $this->fpdf->Cell(100, 8, $cadena, 0,1);
+              $this->fpdf->Cell(100, 8, utf8_decode($cadena), 0,1);
 
               $cadena=$value.' ';
 
