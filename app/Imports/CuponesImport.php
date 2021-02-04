7eb784d9a5798355265ddd82d347586586560177
Orangel Barrera <obarrerafranco@gmail.com>
2021-02-04 09:23:45 -0500

    Ajuste propiedades de cupones

--- app/Imports/CuponesImport.php
+++ app/Imports/CuponesImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\AlpCupones;
+use App\Models\AlpCuponesAlmacen;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 
@@ -33,21 +34,26 @@ class CuponesImport implements ToCollection
             //dd($row);
 
 
-                       AlpCupones::create([
+                     $id =  AlpCupones::create([
                     'codigo_cupon'     => $row[0],
                     'tipo_reduccion'    => $row[1], 
                     'valor_cupon'    => $row[2], 
                     'limite_uso'    => $row[3], 
                     'limite_uso_persona'    => $row[4], 
-                    'fecha_inicio'    => '2020-07-01', 
+                    'fecha_inicio'    => '2021-02-04', 
                     //'fecha_inicio'    => $row[5], 
-                    'fecha_final' => '2020-08-30', 
+                    'fecha_final' => '2021-05-04', 
                     //'fecha_final' => $row[6], 
                     'monto_minimo' => $row[7], 
                     'maximo_productos' => $row[8], 
                     'id_user' => 1, 
                 ]);
     
+                    $condici = AlpCuponesAlmacen::create([
+                    'id_cupon'     => $id->id,
+                    'id_almacen'     => 1,
+                    'id_user'     => 1,
+                    ]);
 
 
                 }
