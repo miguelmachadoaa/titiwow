9d8d70cdf003728aff968f42ca464bbab55eda36
miguelmachadoaa <miguelmachadoaa@gmail.com>
2021-02-19 10:57:21 -0500

    ajuste de recargaiva

--- app/Console/Commands/RecargaIva.php
+++ app/Console/Commands/RecargaIva.php
@@ -51,7 +51,7 @@ class RecargaIva extends Command
     public function handle()
     {
         
-       $ordenes=AlpOrdenes::where('id','>', '13380')->get();
+       $ordenes=AlpOrdenes::where('id','>', '15592')->get();
 
        foreach ($ordenes as $o) {
 
@@ -107,7 +107,7 @@ class RecargaIva extends Command
 
             $base_impuesto=$resto;
 
-            $impuesto=($resto/(1+$valor_impuesto))*$valor_impuesto;
+            $monto_impuesto=($resto/(1+$valor_impuesto))*$valor_impuesto;
 
           }
 
