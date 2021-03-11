aab229dfff0a983111a81de17afa82292a7d97f8
Orangel Barrera <obarrerafranco@gmail.com>
2021-03-11 10:12:05 -0500

    Se crear el tracking de compras por url

--- resources/views/frontend/tracking.blade.php
+++ resources/views/frontend/tracking.blade.php
@@ -35,21 +35,29 @@ Tracking de Ordenes
     <link href="{{ secure_asset('assets/vendors/select2/css/select2-bootstrap.css') }}" rel="stylesheet" />
 
     <style type="text/css">
-        #circulo {
-            width: 10rem;
-            height: 10rem;
-            border-radius: 50%;
-            background: #3DC639;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-        #circulo > p {
-            font-family: sans-serif;
-            color: white;
-            font-weight: bold;
-        }
+        
+        .bs-wizard {margin-top: 40px;}
+
+        /*Form Wizard*/
+        .bs-wizard {border-bottom: solid 1px #e0e0e0; padding: 0 0 10px 0;}
+        .bs-wizard > .bs-wizard-step {padding: 0; position: relative;}
+        .bs-wizard > .bs-wizard-step + .bs-wizard-step {}
+        .bs-wizard > .bs-wizard-step .bs-wizard-stepnum {color: #595959; font-size: 16px; margin-bottom: 5px;}
+        .bs-wizard > .bs-wizard-step .bs-wizard-info {color: #999; font-size: 14px;}
+        .bs-wizard > .bs-wizard-step > .bs-wizard-dot {position: absolute; width: 30px; height: 30px; display: block; background: #90f38d; top: 45px; left: 50%; margin-top: -15px; margin-left: -15px; border-radius: 50%;} 
+        .bs-wizard > .bs-wizard-step > .bs-wizard-dot:after {content: ' '; width: 14px; height: 14px; background: #3DC639; border-radius: 50px; position: absolute; top: 8px; left: 8px; } 
+        .bs-wizard > .bs-wizard-step > .progress {position: relative; border-radius: 0px; height: 8px; box-shadow: none; margin: 20px 0;}
+        .bs-wizard > .bs-wizard-step > .progress > .progress-bar {width:0px; box-shadow: none; background: #90f38d;}
+        .bs-wizard > .bs-wizard-step.complete > .progress > .progress-bar {width:100%;}
+        .bs-wizard > .bs-wizard-step.active > .progress > .progress-bar {width:50%;}
+        .bs-wizard > .bs-wizard-step:first-child.active > .progress > .progress-bar {width:0%;}
+        .bs-wizard > .bs-wizard-step:last-child.active > .progress > .progress-bar {width: 100%;}
+        .bs-wizard > .bs-wizard-step.disabled > .bs-wizard-dot {background-color: #f4f4f4;}
+        .bs-wizard > .bs-wizard-step.disabled > .bs-wizard-dot:after {opacity: 0;}
+        .bs-wizard > .bs-wizard-step:first-child  > .progress {left: 50%; width: 50%;}
+        .bs-wizard > .bs-wizard-step:last-child  > .progress {width: 50%;}
+        .bs-wizard > .bs-wizard-step.disabled a.bs-wizard-dot{ pointer-events: none; }
+        /*END Form Wizard*/
     </style>
 
 @stop
@@ -114,37 +122,38 @@ Tracking de Ordenes
     </div>
     <div class="row">
         <div class="col-md-12">
-            <div style="border:1px solid #f4f4f4; min-height:300px;">
-                <div class="row">
-                    <div class="col-md-2 col-sm-offset-1">
-                        <div style="padding:10px 30px; 20px 30px">
-                            <div id="circulo" class="text-center">
-                                <p>ok</p>
-                            </div>
-                            <h3 class="text-center">Aprobado</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div style="padding:10px 30px; 20px 30px">
-                            <h3 class="text-center">En Preparación</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div style="padding:10px 30px; 20px 30px">
-                            <h3 class="text-center">Asignado</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div style="padding:10px 30px; 20px 30px">
-                            <h3 class="text-center">En Ruta</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div style="padding:10px 30px; 20px 30px">
-                            <h3 class="text-center">Entregado</h3>
-                        </div>
-                    </div>
+            <div style="border:1px solid #f4f4f4; min-height:150px;">
+            <div class="row bs-wizard" style="border-bottom:0;">
+                
+                
+                <div class="col-xs-3 bs-wizard-step @if($history_envio->estatus_envio=='4') active @elseif($history_envio->estatus_envio== '5' || $history_envio->estatus_envio == '6' || $history_envio->estatus_envio == '7') complete  @else disabled @endif"><!-- complete -->
+                  <div class="text-center bs-wizard-stepnum">En Proceso</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
                 </div>
+                
+                <div class="col-xs-3 bs-wizard-step @if($history_envio->estatus_envio=='5') active @elseif($history_envio->estatus_envio== '6' || $history_envio->estatus_envio == '7') complete  @else disabled @endif"><!-- complete -->
+                  <div class="text-center bs-wizard-stepnum">Asignado</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+                </div>
+                
+                <div class="col-xs-3 bs-wizard-step @if($history_envio->estatus_envio=='6') active @elseif($history_envio->estatus_envio== '7') complete  @else disabled @endif"><!-- active -->
+                  <div class="text-center bs-wizard-stepnum">En Ruta</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+                </div>
+
+                <div class="col-xs-3 bs-wizard-step @if($history_envio->estatus_envio=='7') active  @else disabled @endif"><!-- active -->
+                  <div class="text-center bs-wizard-stepnum">Entregado</div>
+                  <div class="progress"><div class="progress-bar"></div></div>
+                  <a href="#" class="bs-wizard-dot"></a>
+
+                </div>
+
+               
+            </div>
+               
             </div>
         </div>
     </div>
