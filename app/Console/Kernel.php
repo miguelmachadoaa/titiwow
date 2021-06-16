9a6bdbaac0ce63c9c8b2b5f22fa0192b6c296226
Orangel Barrera <obarrerafranco@gmail.com>
2021-06-16 15:15:42 -0500

    Ajustes de cron pruebas y hora de kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -97,7 +97,7 @@ class Kernel extends ConsoleKernel
 
 
          
-        $schedule->command('pedidos:enviados')->hourly();
+        $schedule->command('pedidos:enviados')->dailyAt('18:00');
         
         $schedule->command('verificar:pagos')->everyFiveMinutes();
         $schedule->command('verificar:pagoshora')->hourly()->between('6:00', '23:00');
