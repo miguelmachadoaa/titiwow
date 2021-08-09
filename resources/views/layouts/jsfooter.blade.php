f90f997ad488816c13b4427080cca55055dbbc17
miguelmachadoaa <miguelmachadoaa@gmail.com>
2021-08-09 14:12:42 -0500

    MODALUBICACION | Ajuste ubicacion

--- resources/views/layouts/jsfooter.blade.php
+++ resources/views/layouts/jsfooter.blade.php
@@ -142,7 +142,7 @@
 
                     $('.ubicacion_header a').html(ubicacion.city_name+' '+ubicacion.state_name);
 
-                    $('.pmimodal').html('Esta visualizando productos de la ubicación '+ ubicacion.city_name+' '+ubicacion.state_name);
+                    $('.pmimodal').html('Esta visualizando productos de la ubicación,<br> <span style="font-size:16px" > '+ ubicacion.city_name+' '+ubicacion.state_name+'</sapn>');
 
                     $('.addtocart').removeClass('hidden');
 
