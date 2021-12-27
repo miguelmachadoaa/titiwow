27c7c3aca6300dc888d42d3cdae533e56e0fac55
miguelmachadoaa <miguelmachadoaa@gmail.com>
2021-12-27 11:34:39 -0500

    Ajuste buscardor modelo

--- app/Models/AlpProductos.php
+++ app/Models/AlpProductos.php
@@ -24,9 +24,9 @@ class AlpProductos extends Model
         'columns' => [
             'alp_productos.nombre_producto' => 10,
             'alp_productos.referencia_producto' => 5,
-            'alp_productos.presentacion_producto' => 4,
-            'alp_productos.descripcion_corta' => 4,
-            'alp_productos.descripcion_larga' => 4,
+           // 'alp_productos.presentacion_producto' => 4,
+          //  'alp_productos.descripcion_corta' => 1,
+           // 'alp_productos.descripcion_larga' => 4,
             'alp_productos.referencia_producto_sap' => 4,
            // 'alp_productos.descripcion_corta' => 3,
         ]
